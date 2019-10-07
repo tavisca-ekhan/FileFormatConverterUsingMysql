7c9d4266cf8e2bb776236a41f3bf51dadab36dd3
Khan <mdkhan@internal.tavisca.com>
2019-10-07 10:31:44 +0530

    Completed CSV and XML Formatter

--- src/main/java/com/ebran/api/formatter/CSVFormatter.java
+++ src/main/java/com/ebran/api/formatter/CSVFormatter.java
@@ -1,4 +1,21 @@
 package com.ebran.api.formatter;
 
+import com.ebran.api.model.Employee;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+
 public class CSVFormatter {
+
+    public static String format(Object obj) {
+        CsvMapper mapper = new CsvMapper();
+        CsvSchema columns = mapper.schemaFor(Employee.class);
+        try {
+            return mapper.writer(columns).writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        return "";
+    }
 }
--- src/main/java/com/ebran/api/formatter/XMLFormatter.java
+++ src/main/java/com/ebran/api/formatter/XMLFormatter.java
@@ -1,4 +1,44 @@
 package com.ebran.api.formatter;
 
+import com.ebran.api.model.Employee;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+import java.beans.XMLEncoder;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class XMLFormatter {
+
+    public static String format(Object obj) {
+        try {
+            ObjectMapper xmlMapper = new XmlMapper();
+            xmlMapper.enable(SerializationFeature.INDENT_OUTPUT);
+            return xmlMapper.writeValueAsString(obj);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+
+    public static void serializeXML(Employee employee) throws IOException {
+        FileOutputStream out = new FileOutputStream("data.xml");
+        XMLEncoder encoder = new XMLEncoder(new BufferedOutputStream(out));
+        encoder.writeObject(employee);
+        encoder.close();
+        out.close();
+    }
+
+    public static void serializeXML(ArrayList<Employee> employeeArrayList) throws IOException {
+        Employee[] employees = employeeArrayList.toArray(new Employee[employeeArrayList.size()]);
+        FileOutputStream out = new FileOutputStream("data.xml");
+        XMLEncoder encoder = new XMLEncoder(new BufferedOutputStream(out));
+        encoder.writeObject(employees);
+        encoder.close();
+        out.close();
+    }
 }
